a4d4b2e6f3515228d59dc1a61b426be79357bd9f
Dea Dania <dea.dania.business@gmail.com>
2021-10-21 13:39:44 +0700

    body login

--- api/public/public.go
+++ api/public/public.go
@@ -14,7 +14,7 @@ import (
 )
 
 type LoginInput struct {
-	Username string `json:"username" binding:"required"`
+	Email    string `json:"email" binding:"required"`
 	Password string `json:"password" binding:"required"`
 }
 
